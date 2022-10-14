56cd855ffc33033b05bb95712f2e14a7919ae29a
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-10-14 12:04:14 +0200

    Add /reload command

--- Spigot/src/main/java/net/pinger/disguiseplus/executors/DisguisePlusExecutor.java
+++ Spigot/src/main/java/net/pinger/disguiseplus/executors/DisguisePlusExecutor.java
@@ -4,6 +4,8 @@ import com.jonahseguin.drink.annotation.Command;
 import com.jonahseguin.drink.annotation.Require;
 import com.jonahseguin.drink.annotation.Sender;
 import net.pinger.disguiseplus.DisguisePlus;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class DisguisePlusExecutor {
@@ -18,5 +20,12 @@ public class DisguisePlusExecutor {
     @Require(value = "permission.dp.modify")
     public void onCommand(@Sender Player sender) {
         this.dp.getInventoryManager().getDisguisePlusProvider().open(sender);
+    }
+
+    @Command(name = "reload", desc = "Reload the configuration")
+    @Require(value = "permission.dp.reload")
+    public void reload(@Sender CommandSender sender) {
+        this.dp.getFeatureManager().reload();
+        sender.sendMessage(ChatColor.AQUA + "[Hynick] Reloaded configuration files.");
     }
 }
